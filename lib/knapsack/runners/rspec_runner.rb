02035794965207ac5b7a9b50da7579b04f578ad8
Simon Coffey <simon@urbanautomaton.com>
2016-05-19 15:08:33 +0100

    Use Knapsack.logger for runner output
    Currently the different runners use `puts` to directly print their output to
    STDOUT.
    
    This commit changes them to use Knapsack.logger at INFO level. By default this
    will still print to STDOUT as before, but allows users to send this output
    elsewhere, or suppress it with a higher log level if desired.

--- lib/knapsack/runners/rspec_runner.rb
+++ lib/knapsack/runners/rspec_runner.rb
@@ -4,18 +4,17 @@ module Knapsack
       def self.run(args)
         allocator = Knapsack::AllocatorBuilder.new(Knapsack::Adapters::RSpecAdapter).allocator
 
-        puts
-        puts 'Report specs:'
-        puts allocator.report_node_tests
-        puts
-        puts 'Leftover specs:'
-        puts allocator.leftover_node_tests
-        puts
+        Knapsack.logger.info
+        Knapsack.logger.info 'Report specs:'
+        Knapsack.logger.info allocator.report_node_tests
+        Knapsack.logger.info
+        Knapsack.logger.info 'Leftover specs:'
+        Knapsack.logger.info allocator.leftover_node_tests
+        Knapsack.logger.info
 
         cmd = %Q[bundle exec rspec #{args} --default-path #{allocator.test_dir} -- #{allocator.stringify_node_tests}]
 
-        system(cmd)
-        exit($?.exitstatus) unless $?.exitstatus.zero?
+        exec(cmd)
       end
     end
   end
